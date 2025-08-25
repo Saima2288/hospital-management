e873c33ac340219f8ceb382b38b5624862865425
Saima Mehndi <saimamehndi2288@gmail.com>
2025-08-25 23:45:40 +0530

    initial commit backend

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "MERN-HOSPITAL-MANAGEMENT-APP",
+        
+    })
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch((err) => {
+        console.error(`Error occurred while connecting to database: ${err}`);
+        // Optional: You could throw the error or handle it further here
+        // throw err; // Uncomment this if you want to stop execution on error
+    });
+};
